815dd5c2c12e8d6705c56aad20c4fb1a27e5e17e
kedarsubedi <kedarvilliers17@gmail.com>
2022-07-13 18:39:13 +0545

    initial

---
+++ mySqlCrud/Controllers/AlbumsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mySqlCrud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mySqlCrud.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        public IActionResult Index()
+        {
+            MusicStoreContext context = HttpContext.RequestServices.GetService(typeof(mySqlCrud .Models.MusicStoreContext)) as MusicStoreContext;
+
+            return View(context.GetAllAlbums());
+        }
+    }
+}
